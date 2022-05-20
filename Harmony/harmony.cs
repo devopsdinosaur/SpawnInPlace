144e443a21a69ab00b5a6fb50659a3a84479ca60
devopsdinosaur <devopsdinosaur@gmail.com>
2022-05-20 16:29:44 -0400

    Initial working commit of v0.0.1

---
+++ Harmony/harmony.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+
+namespace Harmony {
+
+    public class SpawnInPlace : IModApi {
+        public void InitMod(Mod _modInstance) {
+            Log.Out("Patching Harmony code from {0} into game assembly.", GetType());
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(QuestJournal))]
+    [HarmonyPatch("FailAllActivatedQuests")]
+    public class SpawnInPlace_QuestJournal_FailAllActivatedQuests_Prefix {
+        private static bool Prefix(QuestJournal __instance) {
+            // Disable quest failure for death by skipping over the method entirely
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerMoveController))]
+    [HarmonyPatch("findSpawnPosition")]
+    public class SpawnInPlace_PlayerMoveController_findSpawnPosition_Prefix : PlayerMoveController {
+        private static bool Prefix(PlayerMoveController __instance, ref SpawnPosition __result, EntityPlayer ___entityPlayerLocal, bool _bSpawnAtBedRoll) {
+            // Spawn at current position on death
+            if (GameManager.Instance.IsEditMode() && __instance.respawnReason != RespawnType.Died) {
+                return true;
+            }
+            int x = Utils.Fastfloor(___entityPlayerLocal.position.x);
+            int y = Utils.Fastfloor(___entityPlayerLocal.position.y);
+            int z = Utils.Fastfloor(___entityPlayerLocal.position.z);
+            IChunk chunkFromWorldPos = GameManager.Instance.World.GetChunkFromWorldPos(x, y, z);
+            if (chunkFromWorldPos != null) {
+                if (___entityPlayerLocal.position.y == Constants.cStartPositionPlayerInLevel.y) {
+                    ___entityPlayerLocal.position.y = chunkFromWorldPos.GetHeight(ChunkBlockLayerLegacy.CalcOffset(x, z)) + 1;
+                }
+                __result = new SpawnPosition(___entityPlayerLocal.GetPosition(), ___entityPlayerLocal.rotation.y);
+            }
+            return false;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # SpawnInPlace
+
+## Purpose
+- Player now spawns at death location with a 10-second (configurable in Config/buffs) invulnerability buff for running away
+- Challenge quests no longer auto-fail on death
+- Optional: Uncomment lines in Config/entityclasses to remove death XP penalty
+
+## Mod Installation
+Extract the archive or copy cloned git repo to <7D2D_Home>/Mods dir.  This mod uses Harmony and will only work with A20 and above.
+
+## Building
+
+### Build Prep 
+- For best results, clone the repo directly into <7D2D_Home>/Mods directory.  The project file looks for files using relative paths.
+- If you're modifying and building the code then be sure to install sphereii's 0-SCore mod/framework to enable the -autostart (starts the game after building and loads last savegame)
+
+1. Clone or fork repo from GitHub: https://github.com/devopsdinosaur/SpawnInPlace
+1. Open the .sln file using Visual Studio (Pro or Community 2019+)
+1. Build the project.  It should rebuild the DLL and launch the game with the new mod.
+
+Submit a pull request or send me an email if you have fixed code or new features to add.
+
+## Thanks
+- I used sphereii's guides and code for all the Harmony stuff
+
+## Bugs / Issues / Requests
+Email: devopsdinosaur _a_t_ gmail _d_o_t_ com)
+
